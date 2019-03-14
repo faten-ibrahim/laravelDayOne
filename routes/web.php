abd7cc030387e6e4c1ad65e31fdab904433d6fe4
Faten Ibrahim <faten_elssayad@yahoo.com>
2019-03-14 15:13:19 +0200

    Add middleware only to index page of posts

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/posts', 'PostsController@index')->name('posts.index');
+Route::get('/posts', 'PostsController@index')->name('posts.index')->middleware('auth');
 Route::get('/posts/create', 'PostsController@create')->name('posts.create');
 Route::post('/posts','PostsController@store')->name('posts.store');
 Route::get('/posts/{post}/edit','PostsController@edit')->name('posts.edit');
