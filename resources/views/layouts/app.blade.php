4fa265cf38fa8520db23a70d4a1a063a1dfead2e
Faten Ibrahim <faten_elssayad@yahoo.com>
2019-03-14 15:27:50 +0200

    Add All posts link in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li> <a href="{{ url('/posts') }}">All Posts</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
