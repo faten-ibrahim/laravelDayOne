f20dde5211961bcfeb1c5d01aa14b4220adb93c2
Faten Ibrahim <faten_elssayad@yahoo.com>
2019-03-15 13:15:43 +0200

    add transformation layer by making a resource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -12,6 +12,7 @@ class PostResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
+    
     public function toArray($request)
     {
         return [
